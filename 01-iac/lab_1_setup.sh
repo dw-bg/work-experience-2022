3305c6aca6c192af277d6e2a16161bccf790cb93
Duncan Wright <duncan951@bailliegifford.com>
2022-10-24 13:22:04 +0100

    filepath

--- 01-iac/lab_1_setup.sh
+++ 01-iac/lab_1_setup.sh
@@ -4,5 +4,5 @@ wget https://raw.githubusercontent.com/dw-bg/work-experience-2022/main/01-iac/ma
 wget https://raw.githubusercontent.com/dw-bg/work-experience-2022/main/01-iac/my-variables.tfvars
 mkdir terraform
 mv main.tf terraform/
-mv my-variables.tf terraform/
+mv my-variables.tfvars terraform/
 rm -rf lab_1_setup.sh
