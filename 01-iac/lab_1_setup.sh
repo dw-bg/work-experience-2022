98b1ff761e5b7e6c900ed86b1971d405b98a7534
Duncan Wright <duncan951@bailliegifford.com>
2022-10-24 13:16:33 +0100

    lab setup script

---
+++ 01-iac/lab_1_setup.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+# Lab Setup Script
+wget https://raw.githubusercontent.com/dw-bg/work-experience-2022/main/01-iac/main.tf
+wget https://raw.githubusercontent.com/dw-bg/work-experience-2022/main/01-iac/my-variables.tfvars
+mkdir terraform
+mv main.tf terraform/
+mv my-variables.tf terraform/
+rm -rf lab_1_setup.sh
